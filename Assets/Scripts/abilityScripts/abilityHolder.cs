28e7967c1900e66dedb75d0630d0e90f136a3dd9
DESKTOP-CF3HJCE\sophi <Dohgca@gmail.com>
2021-11-07 22:29:02 +0000

    added basic deity & his skill system tied with UI.

--- Assets/Scripts/abilityScripts/abilityHolder.cs
+++ Assets/Scripts/abilityScripts/abilityHolder.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class abilityHolder : MonoBehaviour
 {
-
+    Image skillImage;
     public KeyCode triggerKey;
 
     public abstractAbility ability;
@@ -19,6 +20,56 @@ public class abilityHolder : MonoBehaviour
     }
 
     AbilityState state = AbilityState.active;
+
+    private void OnEnable()
+    {
+        skillImage = GetComponent<Image>();
+        skillImage.type = Image.Type.Filled;
+        skillImage.fillClockwise = false;
+        skillImage.fillAmount = 1;
+    }
+
+    void onReady()
+    {
+        if (Input.GetKeyDown(triggerKey))
+        {
+            ability.activate(transform.root.gameObject);
+            state = AbilityState.active;
+            activeTime = ability.activeTime;
+        }
+
+    }
+
+    void onActive()
+    {
+        if (activeTime > 0)
+        {
+            activeTime -= Time.deltaTime;
+        }
+        else
+        {
+            state = AbilityState.cooldown;
+            cooldownTime = ability.cooldownTime;
+
+            skillImage.fillAmount = 0;
+        }
+    }
+
+    void onCooldown()
+    {
+        if (cooldownTime > 0)
+        {
+            cooldownTime -= Time.deltaTime;
+
+            skillImage.fillAmount += 1 / cooldownTime * Time.deltaTime;
+        }
+        else
+        {
+            skillImage.fillAmount = 1;
+
+            state = AbilityState.ready;
+        }
+    }
 
     void Update()
     {
@@ -39,42 +90,6 @@ public class abilityHolder : MonoBehaviour
 
         }
 
-
-        void onReady()
-        {
-            if (Input.GetKeyDown(triggerKey))
-            {
-                ability.activate(gameObject);
-                state = AbilityState.active;
-                activeTime = ability.activeTime;
-            }
-            
-        }
-       
-        void onActive()
-        {
-            if (activeTime > 0)
-            {
-                activeTime -= Time.deltaTime;
-            }
-            else
-            {
-                state = AbilityState.cooldown;
-                cooldownTime = ability.cooldownTime;
-            }
-        }
-
-        void onCooldown()
-        {
-            if (cooldownTime > 0)
-            {
-                cooldownTime -= Time.deltaTime;
-            }
-            else
-            {
-                state = AbilityState.ready;
-            }
-        }
-
     }
+
 }
