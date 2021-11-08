42dad2b2c95185af99edc0177542bd6d34873984
DESKTOP-CF3HJCE\sophi <Dohgca@gmail.com>
2021-11-08 12:33:34 +0000

    Implemented janky skill by level enabling.

--- Assets/Scripts/GodController.cs
+++ Assets/Scripts/GodController.cs
@@ -6,13 +6,24 @@ using UnityEngine;
 public class GodController : MonoBehaviourPun
 {
 
-    public float movementSpeed;
+
+
     private PhotonView PV;
+    public Camera godCam;
+
+    public GameObject[] level1Skills;
+    public GameObject[] level2Skills;
+    public GameObject[] level3Skills;
+    public GameObject[] level4Skills;
+
+    public float movementSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         PV = GetComponent<PhotonView>();
+        disableAllSkills();
+
     }
 
     // Update is called once per frame
@@ -22,6 +33,20 @@ public class GodController : MonoBehaviourPun
         {
             BasicMovement();
         }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            enableFirstLevel();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            enableSecondLevel();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            enableThirdLevel();
+            enableFourthLevel();
+        }
     }
 
     void BasicMovement()
@@ -30,4 +55,51 @@ public class GodController : MonoBehaviourPun
         transform.Translate(movementVector * movementSpeed * Time.deltaTime);
     }
 
+    void enableFirstLevel()
+    {
+        foreach(GameObject skill in level1Skills)
+        {
+            skill.SetActive(true);
+        }
+    }
+
+    void enableSecondLevel()
+    {
+        foreach (GameObject skill in level2Skills)
+        {
+            skill.SetActive(true);
+        }
+    }
+
+    void enableThirdLevel()
+    {
+        foreach (GameObject skill in level3Skills)
+        {
+            skill.SetActive(true);
+        }
+    }
+
+    void enableFourthLevel()
+    {
+        foreach (GameObject skill in level4Skills)
+        {
+            skill.SetActive(true);
+        }
+    }
+
+    void disableSkills(GameObject[] skills)
+    {
+        foreach(GameObject skill in skills)
+        {
+            skill.SetActive(false);
+        }
+    }
+
+    void disableAllSkills()
+    {
+        disableSkills(level1Skills);
+        disableSkills(level2Skills);
+        disableSkills(level3Skills);
+        disableSkills(level4Skills);
+    }
 }
