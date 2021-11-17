f02abb68f11a4df90996d0052b373ea4b0d3c023
DESKTOP-CF3HJCE\sophi <Dohgca@gmail.com>
2021-11-17 17:06:51 +0000

    once again i try to fix things

--- Assets/Scripts/CarriableResource.cs
+++ Assets/Scripts/CarriableResource.cs
@@ -17,12 +17,14 @@ public class CarriableResource : MonoBehaviour
 
     public resourceType resource;
 
+    private PhotonView PV;
+
     [SerializeField]
     float resourceAmout = 50f;
 
     void Start()
     {
-        
+        PV = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
@@ -39,4 +41,27 @@ public class CarriableResource : MonoBehaviour
     {
         return resourceAmout;
     }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    transferOwnershipOnTouch(collision);
+    //}
+    //
+    //private void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    transferOwnershipOnTouch(collision);
+    //}
+
+    //private void transferOwnershipOnTouch(Collision2D collision)
+    //{
+    //    if (!PV.IsMine)
+    //    {
+    //        Transform collisionObjectRoot = collision.transform.root;
+    //        if (collisionObjectRoot.CompareTag("Player"))
+    //        {
+    //            Debug.Log("TransferedOwnership");
+    //            PV.TransferOwnership(PhotonNetwork.LocalPlayer);
+    //        }
+    //    }
+    //}
 }
